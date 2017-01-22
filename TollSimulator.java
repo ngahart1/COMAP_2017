26409f6b76fd97da2545cccbc00b851a992dc857
Neil Gahart <ngahart1@jhu.edu>
2017-01-22 16:06:26 -0500

    guts of poisson code for james

--- TollSimulator.java
+++ TollSimulator.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public final class TollSimulator {
     final static int SIMULATION_TIME = 1000;
     final static int TIME_STEP = 1;
+    final static int DISTANCE_UNTIL_PLAZA = 1000;
     final static int TOTAL_DISTANCE = 2000;
+    static Road road;
 
-    public Road setupRoad() {
+    public static Road setupRoad() {
         Road r = new Road(3);
         for (int i = 0; i < 3; i++) {
             r.insert(i, new Lane(i));
@@ -13,19 +16,36 @@ public final class TollSimulator {
         return r;
     }
 
+    /** A method to get a random poisson variable.
+     * @param probInASecond the probability of a car entering
+     * the toll area in any second.
+     * @return a Poisson random variable, with appropriate lambda
+     * parameter.
+     */
+    private static int getPoisson(double probInASecond) {
+        int numberOfExpontentials = 0;
+
+        
+
+        return 
+    }
+
     public static void main(String[] args) {
         System.out.println("CAR_NUMBER,START_TIME,TOTAL_TIME,HAS_EZ_PASS,BOOTH_SELECTED");
         ArrayList<Car> cars = new ArrayList<Car>();
+        road = setupRoad();
         int carNumber = 0;
         for (int t = 0; t < SIMULATION_TIME; t += TIME_STEP) {
             // one new car created each time step
             cars.add(new Car(Math.random() > 0.5, carNumber++, t));
-            for (Car c: cars) {
+            Iterator<Car> iter = cars.iterator();
+            while (iter.hasNext()) {
+                Car c = iter.next();
                 if (c != null) {
                     c.act();
                     if (c.getPosition() > TOTAL_DISTANCE) {
                         System.out.println(c);
-                        cars.remove(c);
+                        iter.remove();
                     }
                 }
             }
